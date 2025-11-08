2d7eff6d7402eea242cf519dc844ab6d87898949
Mohammed ElSaady <m.saady9966@gmail.com>
2025-11-08 21:26:39 +0200

    Set TodaysAppointment view as a default page.

--- DentalCareManagmentSystem.Web/Program.cs
+++ DentalCareManagmentSystem.Web/Program.cs
@@ -110,7 +110,7 @@ app.MapControllerRoute(
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Appointments}/{action=TodaysAppointments}/{id?}");
 
 app.MapRazorPages();
 
